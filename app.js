b47c2e03d16666de05ccfadd1bded6f401d1273c
unknown <sggp.kmiecik@gmail.com>
2020-11-12 18:07:48 +0100

    Added a depenencies and initialize the file - app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+
+module.exports = app;
