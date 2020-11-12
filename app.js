4f7efe0680704f169fd77093d0e67f6bb9323494
unknown <sggp.kmiecik@gmail.com>
2020-11-12 18:23:30 +0100

    Added file about eslint configuration for this project

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 const port = process.env.PORT || 3000;
-
 app.listen(port);
 
 module.exports = app;
