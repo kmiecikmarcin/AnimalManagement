4ec993a895864d87d14cf75792ee8712f16e7ef3
unknown <sggp.kmiecik@gmail.com>
2020-11-19 20:51:17 +0100

    Added model for herd

---
+++ Models/Herd.js
@@ -0,0 +1,38 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = require("../Functions/Database/connectionWithDatabase");
+const Users = require("./Users");
+
+const Herd = sequelize.define(
+  "Herd",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      unique: true,
+      allowNull: false,
+      field: "idHerd",
+    },
+    name: {
+      type: DataTypes.STRING(40),
+      allowNull: false,
+      field: "nameOfHerd",
+    },
+    creationDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      dield: "creationDateOfHerd",
+    },
+  },
+  { timestamps: true }
+);
+
+Users.hasMany(Herd, {
+  foreignKey: {
+    allowNull: false,
+    name: "idUser",
+  },
+});
+
+module.exports = Herd;
