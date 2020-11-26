6837c68df507fa85a4aab38ba5e67f8578cbbdf9
unknown <sggp.kmiecik@gmail.com>
2020-11-26 20:20:36 +0100

    Added 'product from an animal' model

--- Models/AllProductsFromAnimals.js
+++ Models/AllProductsFromAnimals.js
@@ -4,31 +4,35 @@ const sequelize = require("../Functions/Database/connectionWithDatabase");
 const TypesOfProducts = require("./TypesOfProducts");
 const Users = require("./Users");
 
-const AllProductsFromAnimals = sequelize.define("", {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: Sequelize.UUIDV4,
-    primaryKey: true,
-    unique: true,
-    allowNull: false,
-    field: "idProduct",
+const AllProductsFromAnimals = sequelize.define(
+  "AllProductsFromAnimals",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+      unique: true,
+      allowNull: false,
+      field: "idProduct",
+    },
+    quantity: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      field: "quantityOfProduct",
+    },
+    currentQuentity: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      field: "currentQuantityOfProduct",
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      dield: "dateOfAddedProduct",
+    },
   },
-  quantity: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-    field: "quantityOfProduct",
-  },
-  currentQuentity: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-    field: "currentQuantityOfProduct",
-  },
-  date: {
-    type: DataTypes.DATEONLY,
-    allowNull: false,
-    dield: "dateOfAddingProduct",
-  },
-});
+  { timestamps: true }
+);
 
 TypesOfProducts.hasMany(AllProductsFromAnimals, {
   foreignKey: {
