8c02a35137a29758ea3c2b533245ed30772d7305
unknown <sggp.kmiecik@gmail.com>
2020-11-28 18:50:30 +0100

    Implementation function 'userLogin'

--- Functions/Users/userLogin.js
+++ Functions/Users/userLogin.js
@@ -1,6 +1,30 @@
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const { json } = require("body-parser");
+const jwt = require("jsonwebtoken");
 
-function userLogin() {}
+async function userLogin(
+  enteredPasswordFromUser,
+  userPassword,
+  userId,
+  userIdRole,
+  nameOfRole
+) {
+  const match = await bcrypt.compare(enteredPasswordFromUser, userPassword);
+  if (match) {
+    jwt.sign(
+      {
+        id: userId,
+        idTypeOfUserRole: userIdRole,
+        typeOfRole: nameOfRole,
+      },
+      process.env.S3_SECRETKEY,
+      { expiresIn: "36h" },
+      (token) => {
+        json({ token });
+      }
+    );
+  }
+  return json({ Error: "User name or password is incorrect!" });
+}
 
 module.exports = userLogin;
