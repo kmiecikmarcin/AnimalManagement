80303947c35444e0fc037ca7b27c78d2660af680
unknown <sggp.kmiecik@gmail.com>
2020-11-28 13:06:39 +0100

    Added filling data function for types of products feed

--- app.js
+++ app.js
@@ -6,22 +6,25 @@ const Genders = require("./Models/Genders");
 const TypesOfUsersRoles = require("./Models/TypesOfUsersRoles");
 const TypesOfAnimals = require("./Models/TypesOfAnimals");
 const KindOfAnimals = require("./Models/KindOfAnimals");
+const TypesOfProducts = require("./Models/TypesOfProducts");
 const fillDataForGenderTable = require("./Functions/Database/fillDataForGenderTable");
 const fillDataForUsersTypesOfRolesInDatabase = require("./Functions/Database/fillDataForUsersTypesOfRolesInDatabase");
 const fillDataForTypesOfAnimalsTable = require("./Functions/Database/fillDataForTypesOfAnimalsTable");
 const fillDataForKindOfAnimalsTable = require("./Functions/Database/fillDataForKindOfAnimalsTable");
+const fillDataForTypesOfProducts = require("./Functions/Database/fillDataForTypesOfProducts");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 sequelize
-  .sync({ force: false })
+  .sync({ force: true })
   .then(() => {
     fillDataForGenderTable(Genders);
     fillDataForUsersTypesOfRolesInDatabase(TypesOfUsersRoles);
     fillDataForTypesOfAnimalsTable(TypesOfAnimals);
     fillDataForKindOfAnimalsTable(KindOfAnimals, TypesOfAnimals);
+    fillDataForTypesOfProducts(TypesOfProducts);
   })
   .catch((error) => {
     throw new Error(error);
