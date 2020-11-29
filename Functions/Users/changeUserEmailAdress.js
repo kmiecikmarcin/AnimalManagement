f3cf2f7e6a9cc62166c7d5b28cc808d8a0ed31a4
unknown <sggp.kmiecik@gmail.com>
2020-11-29 19:46:47 +0100

    Implementation the function - 'changeUserEmailAdress'

---
+++ Functions/Users/changeUserEmailAdress.js
@@ -0,0 +1,31 @@
+const bcrypt = require("bcrypt");
+const checkUserEmail = require("./checkUserEmail");
+
+async function changeUserEmailAdress(
+  Users,
+  oldUserEmailAdress,
+  newUserEmailAdress,
+  userPassword
+) {
+  const checkUserEmailAdress = await checkUserEmail(Users, oldUserEmailAdress);
+  if (checkUserEmailAdress !== null) {
+    const checkPassword = await bcrypt.compare(
+      userPassword,
+      checkUserEmailAdress.id
+    );
+    if (checkPassword) {
+      const updateUserEmail = Users.update(
+        { email: newUserEmailAdress },
+        { where: { id: checkUserEmailAdress.id } }
+      );
+      if (updateUserEmail) {
+        return updateUserEmail;
+      }
+      return null;
+    }
+    return null;
+  }
+  return null;
+}
+
+module.exports = changeUserEmailAdress;
