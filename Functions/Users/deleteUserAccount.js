b2194597efbcdbc6285c8e30f54c2e80e43a4d75
unknown <sggp.kmiecik@gmail.com>
2020-11-30 17:34:21 +0100

    Finished implementation for delete account

--- Functions/Users/deleteUserAccount.js
+++ Functions/Users/deleteUserAccount.js
@@ -1,4 +1,29 @@
-async function deleteUserAccount() {
+const bcrypt = require("bcrypt");
+
+async function deleteUserAccount(
+  Users,
+  enteredUserPassword,
+  userId,
+  userPassword,
+  accountStatus
+) {
+  if (accountStatus === false) {
+    const checkPassword = await bcrypt.compare(
+      enteredUserPassword,
+      userPassword
+    );
+    if (checkPassword) {
+      const deleteAccount = await Users.update(
+        { accountDeletedStatus: true },
+        { where: { id: userId } }
+      );
+      if (deleteAccount) {
+        return deleteAccount;
+      }
+      return null;
+    }
+    return null;
+  }
   return null;
 }
 
