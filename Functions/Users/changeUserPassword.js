11174982ed7aff3d0a52ccd9f504cc65fff11ca0
unknown <sggp.kmiecik@gmail.com>
2020-11-30 16:31:35 +0100

    Finished first implementation for function 'changeUserPassword'

--- Functions/Users/changeUserPassword.js
+++ Functions/Users/changeUserPassword.js
@@ -1,4 +1,29 @@
-async function changeUserPassword() {
+const bcrypt = require("bcrypt");
+
+async function changeUserPassword(
+  Users,
+  oldUserPassword,
+  newUserPassword,
+  userId
+) {
+  if (oldUserPassword !== newUserPassword) {
+    const checkPassword = await bcrypt.compare(
+      oldUserPassword,
+      userId.password
+    );
+    if (checkPassword) {
+      const hash = await bcrypt.hash(newUserPassword, 8);
+      const changePasswordForUser = Users.update(
+        { password: hash },
+        { where: { id: userId } }
+      );
+      if (changePasswordForUser) {
+        return changePasswordForUser;
+      }
+      return null;
+    }
+    return null;
+  }
   return null;
 }
 
