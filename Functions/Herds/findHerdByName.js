bb0bc44e5275fd001e8748ae60796e181e7bd16f
unknown <sggp.kmiecik@gmail.com>
2020-12-05 14:38:27 +0100

    Finished implementation the function 'findHerdByName'

--- Functions/Herds/findHerdByName.js
+++ Functions/Herds/findHerdByName.js
@@ -1,5 +1,11 @@
-async function findHerdByName() {
-  return null;
+async function findHerdByName(Herds, herdType, userId) {
+  const findHerd = await Herds.findOne({
+    where: { name: herdType, idUser: userId },
+  });
+  if (findHerd === null) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = findHerdByName;
