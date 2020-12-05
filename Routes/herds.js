c660eb74e9afb4cb61ee591987a7cacac18fb444
unknown <sggp.kmiecik@gmail.com>
2020-12-05 12:46:12 +0100

    Added jwt verify to endpoint

--- Routes/herds.js
+++ Routes/herds.js
@@ -2,9 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 require("dotenv").config();
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+const Users = require("../Models/Users");
+const findUserById = require("../Functions/Users/findUserById");
 const verifyToken = require("../Functions/Users/verifyJwtToken");
 
-router.get("/takeAllHerds", verifyToken, () => {});
+router.get("/takeAllHerds", verifyToken, (req, res) => {
+  jwt.verify(req.token, process.env.S3_SECRETKEY, async (error, authData) => {
+    if (error) {
+      res.status(403).json({ Error: "Błąd uwierytelniania!" });
+    } else {
+      const checkUser = await findUserById(Users, authData);
+      if (checkUser !== null) res.status(200);
+    }
+  });
+});
 
 module.exports = router;
