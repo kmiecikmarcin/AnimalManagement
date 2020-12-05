619117c07607f42f214bdddc32618469e31f3110
unknown <sggp.kmiecik@gmail.com>
2020-12-05 12:41:13 +0100

    Initialization endpoint for take all herds

--- Routes/herds.js
+++ Routes/herds.js
@@ -2,5 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 require("dotenv").config();
+// const jwt = require("jsonwebtoken");
+const verifyToken = require("../Functions/Users/verifyJwtToken");
+
+router.get("/takeAllHerds", verifyToken, () => {});
 
 module.exports = router;
