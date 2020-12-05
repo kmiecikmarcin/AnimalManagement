47d6b980d4298b44ec7df10e64c2393ff1f1aaef
unknown <sggp.kmiecik@gmail.com>
2020-12-05 13:07:36 +0100

    Added implementation for endpoint which searching herds for user

--- Routes/herds.js
+++ Routes/herds.js
@@ -4,8 +4,10 @@ const router = express.Router();
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const Users = require("../Models/Users");
-const findUserById = require("../Functions/Users/findUserById");
+const Herds = require("../Models/Herds");
 const verifyToken = require("../Functions/Users/verifyJwtToken");
+const findUserById = require("../Functions/Users/findUserById");
+const findAllUserHerds = require("../Functions/Herds/findAllUserHerds");
 
 router.get("/takeAllHerds", verifyToken, (req, res) => {
   jwt.verify(req.token, process.env.S3_SECRETKEY, async (error, authData) => {
@@ -13,7 +15,18 @@ router.get("/takeAllHerds", verifyToken, (req, res) => {
       res.status(403).json({ Error: "Błąd uwierytelniania!" });
     } else {
       const checkUser = await findUserById(Users, authData);
-      if (checkUser !== null) res.status(200);
+      if (checkUser !== null) {
+        const findHerds = await findAllUserHerds(Herds);
+        if (findHerds !== null) {
+          res.status(201).json({ Herds: findHerds });
+        } else {
+          res.status(404).json({
+            Error: "Użytkownik nie posiada hodowli przypisanych do konta!",
+          });
+        }
+      } else {
+        res.status(404).json({ Error: "Użytkownik nie istnieje!" });
+      }
     }
   });
 });
