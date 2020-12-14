c09ce1312a6e355c35fd334e37a18e5402af3712
unknown <sggp.kmiecik@gmail.com>
2020-12-14 18:23:20 +0100

    Implementation the new function

--- Functions/Herds/findHerdByAnimalType.js
+++ Functions/Herds/findHerdByAnimalType.js
@@ -1,5 +1,11 @@
-async function findHerdByAnimalType() {
-  return null;
+async function findHerdByAnimalType(Herds, herdType, userId) {
+  const findHerd = await Herds.findOne({
+    where: { idKindOfAnimals: herdType, idUser: userId },
+  });
+  if (findHerd === null) {
+    return null;
+  }
+  return findHerd;
 }
 
 module.exports = findHerdByAnimalType;
