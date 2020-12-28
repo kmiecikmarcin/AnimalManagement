ca7ee3657d8c7281762b5cb3e6f9b08edcd4b7fc
unknown <sggp.kmiecik@gmail.com>
2020-12-28 17:30:39 +0100

    Added implementation for the new function - find all join type to the herd

--- Functions/Animals/findAllJoinTypeToTheHerd.js
+++ Functions/Animals/findAllJoinTypeToTheHerd.js
@@ -1,4 +1,10 @@
-async function findAllJoinTypeToTheHerd() {
+async function findAllJoinTypeToTheHerd(TypesOfJoinToTheHerd) {
+  const search = await TypesOfJoinToTheHerd.findAll({
+    attributes: ["name"],
+  });
+  if (search !== null) {
+    return search;
+  }
   return null;
 }
 
