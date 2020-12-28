e6de319272d55adb84e6afab8ba3dfdf2305e81f
unknown <sggp.kmiecik@gmail.com>
2020-12-28 14:40:42 +0100

    Added implementation

--- Functions/Animals/findAnimalGender.js
+++ Functions/Animals/findAnimalGender.js
@@ -1,4 +1,10 @@
-async function findAnimalGender() {
+async function findAnimalGender(GenderOfAnimal, animalGender) {
+  const checkAnimalGender = await GenderOfAnimal.findOne({
+    where: { name: animalGender },
+  });
+  if (checkAnimalGender) {
+    return checkAnimalGender;
+  }
   return null;
 }
 
