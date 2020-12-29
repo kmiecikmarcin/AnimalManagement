33e6c9fa2d9aaeeecc9743347ed4c93281cdb8b8
unknown <sggp.kmiecik@gmail.com>
2020-12-29 18:28:54 +0100

    Rename the function

---
+++ Functions/Animals/findAllAnimalsInHerd.js
@@ -0,0 +1,18 @@
+async function findAllAnimalsInHerd(AnimalsInHerd, herdId) {
+  const search = await AnimalsInHerd.findAll({
+    where: { idHerd: herdId },
+    attributes: [
+      "identityNumberOfAnimal",
+      "breedOfAnimal",
+      "dateOfJoinToTheHerd",
+      "birthDate",
+      "animalWeight",
+    ],
+  });
+  if (search !== null && Object.keys(search).length !== 0) {
+    return search;
+  }
+  return null;
+}
+
+module.exports = findAllAnimalsInHerd;
