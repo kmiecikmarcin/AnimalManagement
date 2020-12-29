89d4b17908da46f9343cfe258e3b93d325e410ea
unknown <sggp.kmiecik@gmail.com>
2020-12-29 18:28:32 +0100

    Added function for taking all new born animals in herd

---
+++ Functions/Animals/findAllNewBornAnimalsInHerd.js
@@ -0,0 +1,12 @@
+async function findAllNewBornAnimalsInHerd(AnimalsBirths, herdId) {
+  const search = await AnimalsBirths.findAll({
+    where: { idHerd: herdId },
+    attributes: ["dateOfBirth", "identityNumber"],
+  });
+  if (search !== null && Object.keys(search).length !== 0) {
+    return search;
+  }
+  return null;
+}
+
+module.exports = findAllNewBornAnimalsInHerd;
