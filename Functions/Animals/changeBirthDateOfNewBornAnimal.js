bc1ec114e9b01280dbe946bd1ea53109dfa7628d
unknown <sggp.kmiecik@gmail.com>
2020-12-29 17:45:25 +0100

    Added function for updating birth date of new born animal

---
+++ Functions/Animals/changeBirthDateOfNewBornAnimal.js
@@ -0,0 +1,24 @@
+async function changeBirthDateOfNewBornAnimal(
+  AnimalsBirths,
+  herdId,
+  identityNumberOfNewBornAnimal,
+  oldBirthDate,
+  newBirthDate
+) {
+  const updateBirthDate = await AnimalsBirths.update(
+    { dateOfBirth: newBirthDate },
+    {
+      where: {
+        identityNumber: identityNumberOfNewBornAnimal,
+        dateOfBirth: oldBirthDate,
+        idHerd: herdId,
+      },
+    }
+  );
+  if (updateBirthDate.length > 1) {
+    return updateBirthDate;
+  }
+  return null;
+}
+
+module.exports = changeBirthDateOfNewBornAnimal;
