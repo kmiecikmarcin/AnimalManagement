79e10d0b1b97b4e6bd049af0387f1ad50b78a858
unknown <sggp.kmiecik@gmail.com>
2020-12-30 17:03:53 +0100

    Rebuild function

---
+++ Functions/Animals/findAnimalByHerdNameAndIdentityNumber.js
@@ -0,0 +1,28 @@
+const Herds = require("../../Models/Herds");
+const KindsOfAnimals = require("../../Models/KindsOfAnimals");
+const GenderOfAnimal = require("../../Models/GenderOfAnimal");
+const TypesOfJoinToTheHerd = require("../../Models/GenderOfAnimal");
+
+async function findAnimalByHerdNameAndIdentityNumber(
+  AnimalsInHerd,
+  herdId,
+  identityNumberOfAnimal
+) {
+  const findAnimal = await AnimalsInHerd.findOne({
+    raw: true,
+    where: { identityNumber: identityNumberOfAnimal, idHerd: herdId },
+    attributes: ["identityNumber", "breed", "joinDate", "birthDate", "weight"],
+    include: [
+      { model: Herds, attributes: ["name"] },
+      { model: KindsOfAnimals, attributes: ["name"] },
+      { model: GenderOfAnimal, attributes: ["name"] },
+      { model: TypesOfJoinToTheHerd, attributes: ["name"] },
+    ],
+  });
+  if (findAnimal) {
+    return findAnimal;
+  }
+  return null;
+}
+
+module.exports = findAnimalByHerdNameAndIdentityNumber;
