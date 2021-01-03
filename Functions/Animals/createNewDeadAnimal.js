7f6c24125d69e78f74bd4c69b3b3698c6f52ffa7
unknown <sggp.kmiecik@gmail.com>
2021-01-03 19:38:26 +0100

    Rebuild function

--- Functions/Animals/createNewDeadAnimal.js
+++ Functions/Animals/createNewDeadAnimal.js
@@ -1,7 +1,5 @@
-const Herds = require("../../Models/Herds");
 const AnimalsInHerd = require("../../Models/AnimalsInHerd");
 const ReasonOfDeath = require("../../Models/ReasonOfDeath");
-const findHerdByName = require("../Herds/findHerdByName");
 const findAnimalByHerdNameAndIdentityNumber = require("./findAnimalByHerdIdAndIdentityNumber");
 const findReasonOfDeathByName = require("./findReasonOfDeathByName");
 const changeLifeStatusOfAnimal = require("./changeLifeStatusOfAnimal");
@@ -9,57 +7,50 @@ const changeLifeStatusOfAnimal = require("./changeLifeStatusOfAnimal");
 async function createNewDeadAnimal(
   res,
   AnimalsDeads,
-  userId,
-  herdName,
+  herdId,
   identityNumberOfAnimal,
   dateOfDeath,
   reasonDeath,
   descriptionOfDeath
 ) {
-  const checkHerdName = await findHerdByName(Herds, herdName, userId);
-  if (checkHerdName) {
-    const checkIdentityNumberOfAnimal = await findAnimalByHerdNameAndIdentityNumber(
-      AnimalsInHerd,
-      checkHerdName.id,
-      identityNumberOfAnimal
+  const checkIdentityNumberOfAnimal = await findAnimalByHerdNameAndIdentityNumber(
+    AnimalsInHerd,
+    herdId,
+    identityNumberOfAnimal
+  );
+  if (checkIdentityNumberOfAnimal) {
+    const checkReasonOfDeath = await findReasonOfDeathByName(
+      ReasonOfDeath,
+      reasonDeath
     );
-    if (checkIdentityNumberOfAnimal) {
-      const checkReasonOfDeath = await findReasonOfDeathByName(
-        ReasonOfDeath,
-        reasonDeath
-      );
-      if (checkReasonOfDeath) {
-        const addNewDeadAnimal = AnimalsDeads.create({
-          identityNumber: checkIdentityNumberOfAnimal.identityNumber,
-          date: dateOfDeath,
-          description: descriptionOfDeath,
-          ReasonOfDeathId: checkReasonOfDeath.id,
-          HerdId: checkHerdName.id,
-        });
-        if (addNewDeadAnimal) {
-          const changeLifeStatus = await changeLifeStatusOfAnimal(
-            AnimalsInHerd,
-            checkHerdName.id,
-            checkIdentityNumberOfAnimal.identityNumber
-          );
-          if (changeLifeStatus) {
-            return changeLifeStatus;
-          }
-          return null;
+    if (checkReasonOfDeath) {
+      const addNewDeadAnimal = AnimalsDeads.create({
+        identityNumber: checkIdentityNumberOfAnimal.identityNumber,
+        date: dateOfDeath,
+        description: descriptionOfDeath,
+        ReasonOfDeathId: checkReasonOfDeath.id,
+        HerdId: herdId,
+      });
+      if (addNewDeadAnimal) {
+        const changeLifeStatus = await changeLifeStatusOfAnimal(
+          AnimalsInHerd,
+          herdId,
+          checkIdentityNumberOfAnimal.identityNumber
+        );
+        if (changeLifeStatus) {
+          return changeLifeStatus;
         }
         return null;
       }
-      return res
-        .status(404)
-        .json({ Error: "Wprowadzona przyczyna śmierci jest nieprawidłowa!" });
+      return null;
     }
-    return res.status(404).json({
-      Error: "Zwierzę numerze identyfikacyjnym nie istnieje!",
-    });
+    return res
+      .status(404)
+      .json({ Error: "Wprowadzona przyczyna śmierci jest nieprawidłowa!" });
   }
-  return res
-    .status(400)
-    .json({ Error: "Hodowla o wprowadzonej nazwie nie istnieje!" });
+  return res.status(404).json({
+    Error: "Zwierzę numerze identyfikacyjnym nie istnieje!",
+  });
 }
 
 module.exports = createNewDeadAnimal;
